7cc7b5472756a9356baf65bd41b2a73ae324a04c
Pushpak Thakur <165506024+thpushpak04@users.noreply.github.com>
2025-02-03 21:18:49 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,11 @@
+public class loop{
+    public static void main(String args[]){
+        for(int line =1; line<=4; line++){ // this takes no. of lines as imput
+            for(int j = 1; j <= line; j++){  // this return no.of characters as an output
+                System.out.print("*");    // print the character
+            }
+            System.out.println(); // print new line
+        }
+
+    }
+}
